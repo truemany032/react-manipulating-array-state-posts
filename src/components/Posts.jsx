4e0c19ea9d2038804723c3f8f1ae04b45a248ece
Mind Sathirasrindh <109655871+mydjolie@users.noreply.github.com>
2023-02-02 10:55:27 +0700

    feat:setting up HH w/ vite

---
+++ src/components/Posts.jsx
@@ -0,0 +1,31 @@
+function Posts() {
+  return (
+    <div class="app-wrapper">
+      <h1 class="app-title">Posts</h1>
+      <div class="post-list">
+        <div class="post-item">
+          <div class="post-header">
+            <h2>Post Title #1</h2>
+            <div class="post-social-media-stats">
+              <span class="stats-topic">Likes: </span>
+              <span class="post-likes">10</span>
+            </div>
+          </div>
+          <p class="post-content">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus
+            vel turpis vestibulum, aliquet ipsum vitae, auctor risus. Morbi
+            tincidunt, leo non molestie consectetur, elit libero faucibus
+            tellus, sed fringilla tortor libero sit amet odio. Maecenas sed ante
+            condimentum mauris euismod pellentesque eu eu justo...
+          </p>
+          <div class="post-actions">
+            <button class="like-button">Like</button>
+            <button class="dislike-button">Dislike</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Posts;
