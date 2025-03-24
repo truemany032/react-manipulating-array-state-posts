8dc002f7d10bb42f5e9ab9733d3270d8fbb8e422
Suriya <true.many.032@gmail.com>
2025-03-24 15:19:22 +0700

    Fix: Delete unused useState

--- src/components/Posts.jsx
+++ src/components/Posts.jsx
@@ -2,7 +2,6 @@ import React, { useState, useEffect } from 'react';
 import {postData} from '../raw-data/post-data';
 function Posts() {
   const [posts, setPosts] = useState([]);
-  const [likes, setLikes] = useState(0);
 
   useEffect(() => 
   {
